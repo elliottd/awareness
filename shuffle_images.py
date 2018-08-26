c92a0e0db4410bc8e36fbf028d2d7c6660412dba
Desmond <des.elliott@gmail.com>
2018-08-26 18:26:19 +0100

    Also create image representations that are entirely the zero vector, require the key arguments

--- shuffle_images.py
+++ shuffle_images.py
@@ -17,7 +17,7 @@ def create_random_shuffle(image_order, image_feats):
     order = dict((k, v) for k, v in zip(indices, image_order))
     shuffled_order = np.random.permutation(indices)  # shuffle and return
 
-    shuffled_feats = np.zeros((image_feats.shape[0], image_feats.shape[1]))
+    shuffled_feats = np.zeros((image_feats.shape[0], image_feats.shape[1])).astype('float32')
     shuffled_names = []
 
     # Create the shuffled image features ndarray and the associated names
@@ -28,23 +28,38 @@ def create_random_shuffle(image_order, image_feats):
     return shuffled_names, shuffled_feats
 
 
+def create_zero_vectors(num_samples, num_feats):
+    '''
+    Creates and returns a zero-initialised num_samples X num_feats matrix.
+
+    Returns:
+        the num_samples X num_feats matrix
+    '''
+    
+    return np.zeros((num_samples, num_feats)).astype('float32')
+
+
 def write_shuffled_filenames(names, i, image_order_filename): 
     with open('{}-random{}'.format(image_order_filename, i), 'w') as handle:
         for n in names:
             handle.write('{}\n'.format(n))
 
 
-
 def main(args):
     image_order = [x.strip() for x in open(args.image_order_file).readlines()]
     image_feats = np.load(args.features_file)
+
+    zeros = create_zero_vectors(image_feats.shape[0], image_feats.shape[1])
+    np.save(open('{}-zeros'.format(args.features_file), 'wb'),
+            zeros,
+            allow_pickle=False)
+
     for i in range(args.num_shuffles):
         names, feats = create_random_shuffle(image_order, image_feats)
         np.save(open('{}-random{}'.format(args.features_file, i), 'wb'),
                 feats,
                 allow_pickle=False)
         write_shuffled_filenames(names, i, args.image_order_file)
-        
 
 
 if __name__ == "__main__":
@@ -52,11 +67,13 @@ if __name__ == "__main__":
     parser.add_argument("--image_order_file",
                         help="Path to file containing the order in which the\
                               images are supposed to be processed",
-                        type=str)
+                        type=str,
+                        required=True)
     parser.add_argument("--features_file",
                         help="Path to file containing the image features\
                               corresponding to the image_order_file",
-                        type=str)
+                        type=str,
+                        required=True)
     parser.add_argument("--num_shuffles", type=int, default=5,
                         help="Number of random shuffles")
     main(parser.parse_args())
